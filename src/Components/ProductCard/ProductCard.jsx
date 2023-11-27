00aaa9d99dceca7e0c77daaa30a66b8d183d28e9
frustrated018 <niloyd018@gmail.com>
2023-11-27 18:09:45 +0600

    Made some dummy data for the products and made the products page

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,27 +1,32 @@
 import { GoTriangleUp, GoTriangleDown } from "react-icons/go";
 
-const ProductCard = () => {
+// TODO: Make the name of the product the view details button and add the up and downvote button at the bottom
+// add report buton either here or pooduct details page
+
+const ProductCard = ({ product }) => {
+  const { name, image, tags, upvoteCount, downvoteCount, shortDescription } =
+    product;
   return (
     <>
-      <a href="#" className="group relative block overflow-hidden">
+      <section className="relative block overflow-hidden">
         <div className="absolute end-4 top-4 z-10 space-y-1">
           {/* Upvote Button */}
           <div className="bg-secondary p-1 ">
             <GoTriangleUp size={20} />
             {/* vote count */}
-            <p className="text-xs">200</p>
+            <p className="text-xs text-center">{upvoteCount}</p>
           </div>
           {/* Down vote button */}
           <div className="bg-secondary p-1">
             {/* vote count */}
-            <p className="text-xs">200</p>
+            <p className="text-xs text-center">{downvoteCount}</p>
             <GoTriangleDown size={20} />
           </div>
         </div>
 
         <img
-          src="https://source.unsplash.com/silver-iphone-6-on-white-table-OxvlDO8RwKg"
-          alt=""
+          src={image}
+          alt={`Picture of ${name}`}
           className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
         />
 
@@ -30,30 +35,19 @@ const ProductCard = () => {
           {/* Tags */}
           {/* TODO: Fix the width of this container to fix the responsive issue */}
           <div className="flex flex-wrap gap-1 w-[300px]">
-            <span className=" bg-accent px-3 py-1.5 text-xs font-medium">
-              New
-            </span>
-            <span className=" bg-accent px-3 py-1.5 text-xs font-medium">
-              nothing new
-            </span>
-            <span className=" bg-accent px-3 py-1.5 text-xs font-medium">
-              somehthing new
-            </span>
-            <span className=" bg-accent px-3 py-1.5 text-xs font-medium">
-              mabybe
-            </span>
-            <span className=" bg-accent px-3 py-1.5 text-xs font-medium">
-              ahh
-            </span>
+            {tags.map((tag, index) => (
+              <span
+                key={index}
+                className="bg-accent px-3 py-1.5 text-xs font-medium uppercase"
+              >
+                {tag}
+              </span>
+            ))}
           </div>
           {/* Product name */}
-          <h3 className="mt-4 text-lg font-medium text-gray-900">
-            iphone-13 SE
-          </h3>
+          <h3 className="mt-4 text-lg font-medium text-gray-900">{name}</h3>
           {/* some sort of description */}
-          <p className="mt-1.5 text-sm text-gray-700">
-            Some sort of Description
-          </p>
+          <p className="mt-1.5 text-sm text-gray-700">{shortDescription}</p>
 
           <form className="mt-4">
             <button className="block w-full rounded bg-primary text-white p-4 text-sm font-medium transition hover:scale-105">
@@ -61,7 +55,7 @@ const ProductCard = () => {
             </button>
           </form>
         </div>
-      </a>
+      </section>
     </>
   );
 };
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,19 +1,30 @@
 import { Helmet } from "react-helmet";
 import NavBar from "../../Components/NavBar/NavBar";
 import ProductCard from "../../Components/ProductCard/ProductCard";
+import { useEffect, useState } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("../../../dummyProducts.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Tech Trends | Products</title>
       </Helmet>
       <NavBar></NavBar>
-      <h2 className=" text-center text-3xl text-red-600 mt-20">
+      <h2 className=" text-center text-3xl text-red-600 mt-10">
         All products will be here
       </h2>
-      <div className="flex justify-center items-center mt-6">
-        <ProductCard></ProductCard>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-screen-xl mx-auto my-20">
+        {products.map((product, idx) => (
+          <ProductCard key={idx} product={product}></ProductCard>
+        ))}
       </div>
     </>
   );
