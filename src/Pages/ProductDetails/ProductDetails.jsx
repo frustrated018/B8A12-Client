71ea87bf1036e573d4a8de6d13c06fae18b1bf4c
frustrated018 <niloyd018@gmail.com>
2023-11-28 00:28:50 +0600

    dstructured and showed the data in the details page

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -8,30 +8,28 @@ import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const { id } = useParams();
-//   fetching data
-      const axiosPublic = useAxiosPublic();
-    const { data: product = [] } = useQuery({
-      queryKey: ["product"],
-      queryFn: async () => {
-        const res = await axiosPublic.get(`/products/details/${id}`);
-        return res.data;
-      },
-    });
+  //   fetching data
+  const axiosPublic = useAxiosPublic();
+  const { data: product = [] } = useQuery({
+    queryKey: ["product"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/products/details/${id}`);
+      return res.data;
+    },
+  });
 
-    console.log(product);
+  const { name, image, tags, upvoteCount, downvoteCount, longDescription } =
+    product;
 
   return (
     <>
       <NavBar></NavBar>
-      <h3 className="text-center text-3xl text-green-500 my-14">
-        Product Details will be here
-      </h3>
       {/* Details section */}
-      <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 w-[90%] lg:w-[70%] mx-auto">
+      <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 w-[90%] lg:w-[70%] mx-auto my-20 shadow-lg">
         {/* Image */}
         <img
           alt="Student"
-          src="https://source.unsplash.com/gray-and-black-laptop-computer-on-surface-Im7lZjxeLhg"
+          src={image}
           className="h-56 w-full object-cover sm:h-full"
         />
         <div className="p-4">
@@ -41,23 +39,33 @@ const ProductDetails = () => {
             <dl className="-my-3 divide-y divide-primary text-sm">
               <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
                 <dt className="font-medium text-gray-900">Product Name</dt>
-                <dd className="text-gray-700 sm:col-span-2">
-                  Name of the product
-                </dd>
+                <dd className="text-gray-700 sm:col-span-2">{name}</dd>
               </div>
 
               <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
                 <dt className="font-medium text-gray-900">Tags</dt>
-                <dd className="text-gray-700 sm:col-span-2">All the tags</dd>
+                <dd className="text-gray-700 sm:col-span-2">
+                  {tags && tags.length > 0 ? (
+                    <div className="flex flex-wrap gap-2">
+                      {tags.map((tag, index) => (
+                        <span
+                          key={index}
+                          className="inline-block bg-accent rounded-lg px-3 py-1 text-sm font-semibold text-black"
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
+                  ) : (
+                    <span>No tags available</span>
+                  )}
+                </dd>
               </div>
 
               <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
                 <dt className="font-medium text-gray-900">Description</dt>
                 <dd className="text-gray-700 sm:col-span-2">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et
-                  facilis debitis explicabo doloremque impedit nesciunt dolorem
-                  facere, dolor quasi veritatis quia fugit aperiam aspernatur
-                  neque molestiae labore aliquam soluta architecto?
+                  {longDescription}
                 </dd>
               </div>
             </dl>
@@ -66,11 +74,11 @@ const ProductDetails = () => {
           <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 w-[90%] mx-auto ">
             <button className="p-3 bg-green-400 rounded-md flex justify-center items-center gap-2">
               <BiSolidUpvote size={24} />
-              <span>100</span>
+              <span>{upvoteCount}</span>
             </button>
             <button className="p-3 bg-orange-400 rounded-md flex justify-center items-center gap-2">
               <BiSolidDownvote size={24} />
-              <span>200</span>
+              <span>{downvoteCount}</span>
             </button>
             <button className="p-3 bg-red-400 rounded-md flex justify-center items-center gap-2">
               <span>Report </span>
