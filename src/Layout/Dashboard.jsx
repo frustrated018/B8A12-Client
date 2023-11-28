7b98f6f08b23406b1e0a5db7591319e1742d67cc
frustrated018 <niloyd018@gmail.com>
2023-11-29 00:02:14 +0600

    Updated the dashboard for modarator links

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -13,57 +13,92 @@ const Dashboard = () => {
   // TODO: Check admin from DB
   const [isAdmin] = useAdmin();
 
-  //   TODO: Make a modarator route as well
-  //   const isModarator = false;
+  // TODO: Make a moderator route as well
+  const isModerator = true;
+
+  // Admin links
+  const adminLinks = (
+    <>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/userProfile">
+          <ImStatsDots />
+          Site Stats
+        </NavLink>
+      </li>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/manageusers">
+          <FaUsersCog />
+          Manage Users
+        </NavLink>
+      </li>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/myproducts">
+          <AiFillGift />
+          Manage Coupon
+        </NavLink>
+      </li>
+    </>
+  );
+
+  // Moderator links
+  const moderatorLinks = (
+    <>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/moderatorhome">
+          <FaHome></FaHome>
+          Moderator Home
+        </NavLink>
+      </li>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/pendingposts">
+          <FaBook></FaBook>
+          Pending Posts
+        </NavLink>
+      </li>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/reportedposts">
+          <FaBook></FaBook>
+          Reported Posts
+        </NavLink>
+      </li>
+    </>
+  );
+
+  // Common user links
+  const commonUserLinks = (
+    <>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/userProfile">
+          <FaUser></FaUser>
+          My Profile
+        </NavLink>
+      </li>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/addproduct">
+          <MdOutlineNoteAdd />
+          Add Product
+        </NavLink>
+      </li>
+      <li className="bg-primary rounded-lg">
+        <NavLink to="/dashboard/myproducts">
+          <BsCollectionFill />
+          My Products
+        </NavLink>
+      </li>
+    </>
+  );
+
   return (
     <>
       <div className="flex">
         {/* dashboard side bar */}
         <div className="w-64 min-h-screen bg-secondary">
           <ul className="menu p-4 text-white space-y-1">
-            {isAdmin ? (
-              <>
-                <li className="bg-primary rounded-lg">
-                  <NavLink to="/dashboard/userProfile">
-                    <ImStatsDots />
-                    Site Stats
-                  </NavLink>
-                </li>
-                <li className="bg-primary rounded-lg">
-                  <NavLink to="/dashboard/manageusers">
-                    <FaUsersCog />
-                    Manage Users
-                  </NavLink>
-                </li>
-                <li className="bg-primary rounded-lg">
-                  <NavLink to="/dashboard/myproducts">
-                    <AiFillGift />
-                    Manage Cupon
-                  </NavLink>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="bg-primary rounded-lg">
-                  <NavLink to="/dashboard/userProfile">
-                    <FaUser></FaUser>
-                    My Profile
-                  </NavLink>
-                </li>
-                <li className="bg-primary rounded-lg">
-                  <NavLink to="/dashboard/addproduct">
-                    <MdOutlineNoteAdd />
-                    Add Product
-                  </NavLink>
-                </li>
-                <li className="bg-primary rounded-lg">
-                  <NavLink to="/dashboard/myproducts">
-                    <BsCollectionFill />
-                    My Products
-                  </NavLink>
-                </li>
-              </>
-            )}
+            {isAdmin
+              ? adminLinks
+              : isModerator
+              ? moderatorLinks
+              : commonUserLinks}
 
             {/* shared nav links */}
             <div className="divider"></div>
