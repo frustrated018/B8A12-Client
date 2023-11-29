7ccafedd8a86296b45af7023ce7363a404176861
frustrated018 <niloyd018@gmail.com>
2023-11-29 12:21:51 +0600

    Redesigned the user profile page

--- src/Pages/Dashboard/UserProfile/UserProfile.jsx
+++ src/Pages/Dashboard/UserProfile/UserProfile.jsx
@@ -1,31 +1,33 @@
 import { RiVerifiedBadgeFill } from "react-icons/ri";
 import useAuth from "../../../Hooks/useAuth";
+
 const UserProfile = () => {
-    const {user} = useAuth();
+  const { user } = useAuth();
+
   return (
-    <>
-      <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
+    <div className="flex flex-col items-center p-8">
+      <div className="max-w-md bg-white rounded-lg overflow-hidden shadow-md">
         <img
           alt="user profile"
           src={user.photoURL}
-          className="h-full w-full object-cover"
+          className="h-64 w-full object-cover"
         />
-        <div className="p-8">
-          <div className="mx-auto max-w-xl text-left">
-            <div>
-              <h2 className="text-3xl font-bold">{user.displayName}</h2>
-              <h2 className="text-3xl my-5 font-bold">{user.email}</h2>
-              <div className="flex items-center bg-green-400 w-[20%] rounded-lg">
-                <RiVerifiedBadgeFill size={24}/> 
-                <span className="text-2xl my-4">Verified</span>
-              </div>
-            </div>
-
-            <button className="p-4 bg-primary hover:bg-accent rounded-lg mt-5 text-white text-xl font-bold">Get veified</button>
+        <div className="p-6">
+          <h2 className="text-2xl font-bold mb-2">{user.displayName}</h2>
+          <p className="text-gray-600">{user.email}</p>
+          <div className="flex items-center mt-4">
+            <RiVerifiedBadgeFill size={24} className="text-green-500 mr-2" />
+            <span className="text-lg font-semibold text-green-500">
+              Verified
+            </span>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+
+      <button className="mt-8 p-4 bg-primary hover:bg-accent rounded-lg text-white text-xl font-bold">
+        Get Verified
+      </button>
+    </div>
   );
 };
 
